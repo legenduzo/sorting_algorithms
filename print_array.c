ff67772a934189fc328428ebc4f61d4a2ddd2554
Kgothatso2 <kgothatso.m1997@gmail.com>
2023-11-21 03:06:20 -0800

    0-O

--- print_array.c
+++ print_array.c
@@ -14,9 +14,11 @@ void print_array(const int *array, size_t size)
 		i = 0;
 		while (array && i < size)
 		{
-			if (i > 0)
+			if(i > 0)
+			{
 			    printf(", ");
-			printf("%d", array [i]);
+			}
+			printf("%d", array[i]);
 			++i;
 		}
 		printf("\n");
