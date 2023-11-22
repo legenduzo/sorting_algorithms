80915ee82a32518b0814d30f3a242f044ebb5552
Precious Aleaji <legenduz15@gmail.com>
2023-11-22 02:55:15 +0100

    Quick sort
    Co-authored by: Kgothatso2 <kgothatso.m1997@gmail.com>

---
+++ 3-quick_sort.c
@@ -0,0 +1,70 @@
+#include "sort.h"
+
+/**
+ * lomuto - implements the lomuto partition scheme
+ * @array: the array to process
+ * @high: high
+ * @low: low
+ * @size: size of the array
+ *
+ * Return: int
+ */
+
+int lomuto(int *array, int low, int high, size_t size)
+{
+	int pivot, i, j, a;
+
+	pivot = array[high];
+	i = low;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			a = array[i];
+			array[i] = array[j];
+			array[j] = a;
+			if (i != j)
+				print_array(array, size);
+			i++;
+		}
+	}
+	array[high] = array[i];
+	array[i] = pivot;
+	if (high != i)
+		print_array(array, size);
+	return i;
+}
+
+/**
+ * qs_recursive - recursive call to quick sort
+ * @array: the array
+ * @low: low
+ * @high: high
+ * @size: size of array
+ */
+
+void qs_recursive(int *array, int low, int high, size_t size)
+{
+	int pivot;
+	if (low < high)
+	{
+		pivot = lomuto(array, low, high, size);
+		qs_recursive(array, low, pivot - 1, size);
+		qs_recursive(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - sorts an array using the quicksort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+
+	qs_recursive(array, 0, size - 1, size);
+}
